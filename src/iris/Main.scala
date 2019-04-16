368e5f42b84e873d0715fa9567c60729b5835378
ShiShusheng <2855845850@qq.com>
2019-04-16 18:30:23 +0800

    线性支持向量机

--- src/iris/Main.scala
+++ src/iris/Main.scala
@@ -1,7 +1,7 @@
 package iris
 
 import org.apache.spark.SparkConf
-import org.apache.spark.ml.classification.NaiveBayes
+import org.apache.spark.ml.classification.LinearSVC
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.sql.SparkSession
 
@@ -20,7 +20,6 @@ object Main {
     spark.sparkContext.setLogLevel("WARN") ///日志级别
 
     val file = spark.read.format("csv").load("iris.data")
-    // file.show()
 
     import spark.implicits._
     val random = new Random()
@@ -38,17 +37,19 @@ object Main {
         row.getString(3).toDouble,
         label,
         random.nextDouble())
-    }).toDF("_c0", "_c1", "_c2", "_c3", "label", "rand").sort("rand") //.where("label = 1 or label = 0")
+    }).toDF("_c0", "_c1", "_c2", "_c3", "label", "rand").sort("rand")
+      .where("label = 1 or label = 0")
 
     val assembler = new VectorAssembler().setInputCols(Array("_c0", "_c1", "_c2", "_c3")).setOutputCol("features")
 
     val dataset = assembler.transform(data)
     val Array(train, test) = dataset.randomSplit(Array(0.8, 0.2))
 
-    //bayes
-    val bayes = new NaiveBayes().setFeaturesCol("features").setLabelCol("label")
-    val model = bayes.fit(train) //训练数据集进行训练
-    model.transform(test).show() //测试数据集进行测试，看看效果如何
+    // SVM
+    val svm = new LinearSVC().setMaxIter(20).setRegParam(0.1)
+      .setFeaturesCol("features").setLabelCol("label")
+    val model = svm.fit(train)
+    model.transform(test).show()
   }
 }
 
